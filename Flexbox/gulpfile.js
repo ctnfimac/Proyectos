cf1c7ee757251e9b72b22fbbda39dc46745d83f1
christian <christianipc.1987@gmail.com>
2018-05-01 18:31:13 -0300

    Agrego browser-sync , queda solucionar el lograr un solo css a partir de varios scss,y agregar uglify y pump para la compresion de archivos css

--- Flexbox/gulpfile.js
+++ Flexbox/gulpfile.js
@@ -1,17 +1,31 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var pug = require('gulp-pug');
-	
+var browserSync = require('browser-sync').create();
 
-// gulp.task('default', function(){
-// 	console.log('Hola mundo!!!');
-// });
+// para browserSync
+gulp.task('serve', ['sass'], function() {
+	
+		browserSync.init({
+			server: "./public"
+		});
+	
+		gulp.watch("dev/scss/*.scss", ['sass']);
+		
+		// para que cuando se realice un cambio en el archivo css, se refresque 
+		// el navegador
+		gulp.watch("public/css/main.css").on('change', browserSync.reload);
+		gulp.watch("public/index.html").on('change', browserSync.reload);
+	});
+
 
 gulp.task('sass', function(){
 	return gulp.src('dev/scss/*.scss')
 		.pipe(sass())
-		.pipe(gulp.dest('public/css'));
+		.pipe(gulp.dest('public/css'))
+		.pipe(browserSync.stream());
 });
+
 
 gulp.task('pug',function (){
 	return gulp.src('dev/index.pug')
@@ -22,9 +36,14 @@ gulp.task('pug',function (){
 });
 
 
-gulp.task('default',function(){
+gulp.task('default', ['serve'],function(){
 	gulp.watch('dev/scss/*.scss',['sass']);
 	gulp.watch('dev/index.pug',['pug']);
 });
+
+
+
+
+
 
 
